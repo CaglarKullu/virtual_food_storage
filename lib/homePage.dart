8375878eb50e6efd43f497c1a49525c3f2e65923
Caglar Kullu <caglarkullu@gmail.com>
2023-12-01 16:09:01 +0000

    first commit

---
+++ lib/homePage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StoregeHomepage extends StatelessWidget {
+  const StoregeHomepage({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
