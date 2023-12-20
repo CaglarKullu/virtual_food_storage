44303709b12658ff8e3c42398cd3de2b4ef46c48
Caglar Kullu <caglarkullu@gmail.com>
2023-12-20 12:11:43 +0000

    add: firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,7 +7,9 @@ import 'src/views/login_view.dart';
 import 'src/views/signup_view.dart';
 import 'src/views/welcome_view.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const ProviderScope(child: MyApp()));
 }
 
