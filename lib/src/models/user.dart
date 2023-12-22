dc19458774596a533cfc0e4b6003ada01a74b1bb
Caglar Kullu <caglarkullu@gmail.com>
2023-12-22 11:48:12 +0000

    add: add vault to user

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -1,16 +1,25 @@
 import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart' as fire_base_auth;
+import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart' as supabase;
 
+@immutable
 class AppUser {
   final String id;
   final String email;
   final String? name;
   final String? profileImageUrl;
-  // Add other common attributes as needed
+  final List<String> ownedVaults; // IDs of vaults owned by the user
+  final List<String> sharedVaults; // IDs of vaults shared with the user
 
-  AppUser(
-      {required this.id, required this.email, this.name, this.profileImageUrl});
+  const AppUser({
+    required this.id,
+    required this.email,
+    this.name,
+    this.profileImageUrl,
+    this.ownedVaults = const [],
+    this.sharedVaults = const [],
+  });
 
   // Constructor for Firebase User
   factory AppUser.fromFirebaseUser(fire_base_auth.User firebaseUser) {
@@ -47,6 +56,8 @@ class AppUser {
       'email': email,
       'name': name,
       'profileImageUrl': profileImageUrl,
+      'ownedVaults': ownedVaults,
+      'sharedVaults': sharedVaults,
     };
   }
 
@@ -57,13 +68,34 @@ class AppUser {
       email: map['email'] as String,
       name: map['name'] as String?,
       profileImageUrl: map['profileImageUrl'] as String?,
+      ownedVaults: List<String>.from(map['ownedVaults'] ?? []),
+      sharedVaults: List<String>.from(map['sharedVaults'] ?? []),
     );
   }
 
-  // Optional: Create a method to convert User object to JSON String
+  // Convert User object to JSON String
   String toJson() => json.encode(toMap());
 
-  // Optional: Create a method to create a User object from a JSON String
+  // Create a User object from a JSON String
   factory AppUser.fromJson(String source) =>
       AppUser.fromMap(json.decode(source));
+
+  // copyWith method for immutability
+  AppUser copyWith({
+    String? id,
+    String? email,
+    String? name,
+    String? profileImageUrl,
+    List<String>? ownedVaults,
+    List<String>? sharedVaults,
+  }) {
+    return AppUser(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      profileImageUrl: profileImageUrl ?? this.profileImageUrl,
+      ownedVaults: ownedVaults ?? this.ownedVaults,
+      sharedVaults: sharedVaults ?? this.sharedVaults,
+    );
+  }
 }
