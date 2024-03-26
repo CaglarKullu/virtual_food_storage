d0b6ba427a0a2c1c31a638ba81849f318feab999
Caglar Kullu <97615706+CaglarKullu@users.noreply.github.com>
2024-03-26 14:24:23 +0000

    Update supabase_init.dart

--- lib/supabase_init.dart
+++ lib/supabase_init.dart
@@ -1,8 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-const String supabaseUrl = 'https://ukcfmbxwbjllkpozsnts.supabase.co';
+const String supabaseUrl = '';
 const String supabaseAnonKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrY2ZtYnh3YmpsbGtwb3pzbnRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE0NDcwODgsImV4cCI6MjAxNzAyMzA4OH0.Yix4r68V-MGrd-pjwKlRA-ywcuvkJetVIOqVQ8DC2QY';
+    '';
 
 final SupabaseClient supabaseClient =
     SupabaseClient(supabaseUrl, supabaseAnonKey);
